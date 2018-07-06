f7815a6d855fd787743a05df0642d38d2412b0ae
Caleb Wolfson-Seeley <cwolfsonseeley@BERKELEY.EDU>
2018-07-06 11:15:29 -0400

    create regional school report

---
+++ R/regional_school_report.R
@@ -0,0 +1,26 @@
+#' Regional School Report
+#'
+#' This function creates a Regional School Report for specified MSA and graduating school and outputs the report as an html file.
+#' @param msa MSA code from CDW
+#' @param msa_name name you'd like to give MSA as it appears in the report
+#' @param school_code school code from CDW
+#' @param school_name school name as you'd like it to appear in the report
+#' @param from start date in yyyymmdd format
+#' @param to end date in yyyymmdd format
+#' @param output file location and name for report
+#' @export
+#' @examples
+#' # This would output the report for Boston and the College of Engineering from January 1, 2014 to June 30, 2018
+#' school_report(msa = 14460, msa_name = "Boston", msa = school = EN, school_name = "College of Engineering", from = 20140101, to = 20180630, output = "my_file/engineering_report")
+
+regional_school_report = function(msa, msa_name, school_code, school_name, from, to, output) {
+  rmarkdown::render(
+    "R:/Prospect Development/Prospect Analysis/regionalreports/R/region_and_school_analysis_template.Rmd", params = list(
+      msa = msa,
+      msa_name = msa_name,
+      from = from,
+      to = to
+    ),
+    output_file = paste0(output, ".html")
+  )
+}
